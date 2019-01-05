8bef4eb7a6ba98a74e3e4ef2e6f4e68411bdbf69
loraine214 <35474962+loraine214@users.noreply.github.com>
2019-01-05 05:19:12 -0800

    Create sofia.php

---
+++ sofia.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>description</title>
+	<link rel="stylesheet" type="text/css" href="css/zuri.css">
+</head>
+<body>
+	<div id="main">
+		<div id="base">
+			<img src="img/sofa/shead.jpeg">
+		</div>
+		<div>
+			<p class="size">Height (cm) &nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;90</p>
+			<p class="size1">width (cm) &nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;208</p>
+			<p class="size2">Depth (cm) &nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;99</p>
+		</div>
+		<div>
+			<h1>Description</h1>
+			<p class="uptext">Make this modern and elegant sofa the heart <br> of your living room
+			with soft curves that you <br> will love to sink in to.</p>
+			<ul>
+				<li class="text1">Comes with foam-filled seat cushios for a beautiful look <br> that 
+				bounces back-with no need to plump</li>
+				<li>Comes with 2 free accent scatter cushions, in the colour <br> of your
+				choice</li>
+				<li>Available to purchase online as shown</li>
+				<li>Delivery charges apply</li>
+			</ul>
+			<h2>Zuri Sofa</h2>
+			<p class="price">Only <span>&#8369</span>5999.00</p>
+			<p class="cart"><a href="">Add to cart</a></p>
+			<p class="cancel"><a href="index.php">x</a></p>
+		</div>
+	</div>
+</body>
+</html>
